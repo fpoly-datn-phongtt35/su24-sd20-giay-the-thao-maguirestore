7625d048dcbb439aed8af94916356fd1efd80da4
hoank1704 <hoank1704@gmail.com>
2024-06-07 14:08:25 +0700

    khanhhoa-commit api getAllUsers

--- maguirestore/src/main/java/com/datn/maguirestore/controller/UserController.java
+++ maguirestore/src/main/java/com/datn/maguirestore/controller/UserController.java
@@ -4,25 +4,29 @@ import com.datn.maguirestore.config.Constants;
 import com.datn.maguirestore.dto.AdminUserDTO;
 import com.datn.maguirestore.dto.UserDTO;
 import com.datn.maguirestore.entity.User;
-import com.datn.maguirestore.errors.BadRequestAlertException;
 import com.datn.maguirestore.errors.EmailAlreadyUsedException;
 import com.datn.maguirestore.errors.LoginAlreadyUsedException;
-import com.datn.maguirestore.payload.request.UserCreateRequest;
-import com.datn.maguirestore.payload.request.UserUpdateRequest;
 import com.datn.maguirestore.repository.UserRepository;
 import com.datn.maguirestore.security.jwt.JwtUtils;
 import com.datn.maguirestore.security.services.UserDetailsImpl;
 import com.datn.maguirestore.service.UserService;
 import com.datn.maguirestore.util.HeaderUtil;
+import com.datn.maguirestore.util.PaginationUtil;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -30,10 +34,30 @@ import javax.validation.constraints.Pattern;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Slf4j
 public class UserController {
+
+    private static final List<String> ALLOWED_ORDERED_PROPERTIES = Collections.unmodifiableList(
+            Arrays.asList(
+                    "id",
+                    "login",
+                    "firstName",
+                    "lastName",
+                    "email",
+                    "activated",
+                    "createdBy",
+                    "createdDate",
+                    "lastModifiedBy",
+                    "lastModifiedDate"
+            )
+    );
+
     @Autowired
     private UserService userService;
     @Autowired
@@ -68,9 +92,27 @@ public class UserController {
     }
 
     @SecurityRequirement(name = "Bearer Authentication")
-    @PostMapping("/users")
+    @GetMapping("/users")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<User> createUser(@Valid @RequestBody UserCreateRequest userCreateRequest) throws URISyntaxException {
+    public ResponseEntity<List<AdminUserDTO>> getAllUsers(Pageable pageable) {
+        log.debug("REST request to get all User for an admin");
+        if (!onlyContainsAllowedProperties(pageable)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        final Page<AdminUserDTO> page = userService.getAllUsers(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+    }
+
+    private boolean onlyContainsAllowedProperties(Pageable pageable) {
+        return pageable.getSort().stream().map(Sort.Order::getProperty).allMatch(ALLOWED_ORDERED_PROPERTIES::contains);
+    }
+
+    @SecurityRequirement(name = "Bearer Authentication")
+    @PostMapping("/")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<User> createUser(@Valid @RequestBody AdminUserDTO userCreateRequest) throws URISyntaxException {
 
         if (userRepository.findByLogin(userCreateRequest.getLogin().toLowerCase()).isPresent()) {
             throw new LoginAlreadyUsedException();
@@ -95,8 +137,8 @@ public class UserController {
     // Update user
     @SecurityRequirement(name = "Bearer Authentication")
     @PutMapping(value = "/update/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UserUpdateRequest userUpdateRequest, Principal principal) {
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @Valid @RequestBody AdminUserDTO userUpdateRequest, Principal principal) {
         try {
             String currentUsername = principal.getName();
             User currentUser = userService.findByUsername(currentUsername);
@@ -109,7 +151,7 @@ public class UserController {
             User updatedUser = userService.updateUser(userUpdateRequest, currentUser, targetUser);
             return new ResponseEntity<>(updatedUser, HttpStatus.OK);
         } catch (AccessDeniedException e) {
-            return new ResponseEntity<>("Access Denied.", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("You do not have permission to update this user.", HttpStatus.FORBIDDEN);
         } catch (Exception e) {
             return new ResponseEntity<>("Update failed.", HttpStatus.BAD_REQUEST);
         }
