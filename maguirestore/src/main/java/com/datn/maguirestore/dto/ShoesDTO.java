cc64c1b47649654862a373a77acb445d5642a9b5
tuyenpv <tuyenphamk202@gmail.com>
2024-07-09 17:24:08 +0700

    tuyen-commit[update size,cate]

--- maguirestore/src/main/java/com/datn/maguirestore/dto/ShoesDTO.java
+++ maguirestore/src/main/java/com/datn/maguirestore/dto/ShoesDTO.java
@@ -2,15 +2,14 @@ package com.datn.maguirestore.dto;
 
 import java.io.Serializable;
 import java.util.Objects;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class ShoesDTO implements Serializable {
 
     private Long id;
@@ -20,36 +19,36 @@ public class ShoesDTO implements Serializable {
     private BrandDTO brand;
     private CategoryDTO category;
 
-    @Override
-    public String toString() {
-        return "ShoesDTO{" +
-                "id=" + id +
-                ", code='" + code + '\'' +
-                ", name='" + name + '\'' +
-                ", status=" + status +
-                ", brand=" + brand +
-                ", category=" + category +
-                '}';
-    }
+   @Override
+   public String toString() {
+       return "ShoesDTO{" +
+               "id=" + id +
+               ", code='" + code + '\'' +
+               ", name='" + name + '\'' +
+               ", status=" + status +
+               ", brand=" + brand +
+               ", category=" + category +
+               '}';
+   }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ShoesDTO shoesDTO)) {
-            return false;
-        }
+   @Override
+   public boolean equals(Object o) {
+       if (this == o) {
+           return true;
+       }
+       if (!(o instanceof ShoesDTO shoesDTO)) {
+           return false;
+       }
 
-        if (this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, shoesDTO.id);
-    }
+       if (this.id == null) {
+           return false;
+       }
+       return Objects.equals(this.id, shoesDTO.id);
+   }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
+   @Override
+   public int hashCode() {
+       return Objects.hash(this.id);
+   }
 
 }
